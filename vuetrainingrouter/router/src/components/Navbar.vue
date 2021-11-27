5d0483ce9b703cad9e12f478509823f55640da96
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2021-11-27 20:34:34 +0800

    vue router parameter

--- vuetrainingrouter/router/src/components/Navbar.vue
+++ vuetrainingrouter/router/src/components/Navbar.vue
@@ -5,6 +5,7 @@
 			<li><router-link to="/">Home</router-link></li>
 			<li><router-link to="/about">About</router-link></li>
 			<li><router-link to="/contact">Contact</router-link></li>
+			<li><router-link to="/Profile">Profile</router-link></li>
 		</ul>
 		</nav>
 	</div>
---
+++ vuetrainingrouter/router/src/components/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="profile">
+    <h1>Profile  {{UserId}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data () {
+    return {
+      UserId: this.$route.params.user_id
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2{
+    color: aqua;
+}
+</style>
