aa783f6ba7967924c8d40ef279bd155eb6fdd41e
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2021-11-27 19:54:35 +0800

    vue route

---
+++ vuetrainingrouter/router/src/components/Contact.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="contact">
+    <h1>Contact</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
